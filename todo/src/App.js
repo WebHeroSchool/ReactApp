c2d612ea5371553984e33d615b4d93efeddc7a88
Sveta Mitukhina <s.tu.tu.908@gmail.com>
2019-11-21 23:23:09 +0300

    Work#11: Что такое JSX?

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,14 +1,54 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+const  someVariable = 'Hello!';
+const logics = true;
+const ternaryOperator = false;
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
+        {/* добавлениe в разметку объекта style; */}
+        <p style ={{
+            color: 'lightblue',
+          }}>
+          Hello, World!
+        </p>
+
+        {/* добавление числа: */}
+          <p>
+            {45}
+          </p>
+
+        {/* вывод значения переменной {}; */}
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        {someVariable}
+        </p>
+
+        {/* добавление арифметической операции: */}
+        <p>
+        {20+14}
+        </p>
+        
+        {/* добавление результата логической операции */}
+        <p>
+          {logics && 'Logics is true'}
+        </p>
+
+        {/* добавление результата тернарного условия; */}
+        <p>
+          {ternaryOperator ? 'Ternary is true' : 'Ternary is false'}
+        </p>
+
+        {/* добавление объектов: */}
+        <p>
+          {null}
+          {undefined}
+          {false}
+          {true}
         </p>
         <a
           className="App-link"
