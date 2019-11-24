43e1b6e89bdf6c32a49082b09b136a78d762eb5f
Sveta Mitukhina <s.tu.tu.908@gmail.com>
2019-11-24 13:20:49 +0300

    #21: Вывод списка элементов и ключи

---
+++ todo/src/Components/App/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ItemList from '../ItemList/ItemList'
+import Footer from  '../Footer/Footer'
+import Item from "../Item/Item";
+
+const App = () => {
+
+    const items = [
+        {value: 'Написать новое приложение'},
+        {value: 'Выучить React'},
+        {value: 'Найти работу'}
+    ];
+
+   return ( <div>
+        <h1>Hello!</h1>
+        <ItemList items = {items}/>
+        <Footer count = {3} />
+    </div>);
+}
+export default App;
