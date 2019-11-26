3e92d75c9ee8362615f6072ec72b82e1ae44b941
Sveta Mitukhina <s.tu.tu.908@gmail.com>
2019-11-26 18:21:12 +0300

    #24: classnames

--- todo/src/Components/App/App.js
+++ todo/src/Components/App/App.js
@@ -2,19 +2,22 @@ import React from 'react';
 import ItemList from '../ItemList/ItemList'
 import Footer from  '../Footer/Footer'
 import Item from "../Item/Item";
+import styles from './App.module.css';
+import classnames from 'classnames';
+
 
 const App = () => {
 
     const items = [
-        {value: 'Написать новое приложение'},
-        {value: 'Выучить и понять React'},
-        {value: 'Найти работу'}
+        {value: 'Начать изучать React', isDone: true},
+        {value: 'Попробовать написать первое приложение', isDone: true},
+        {value: 'Дойти до финальной работы', isDone: false}
     ];
 
-   return ( <div>
-        <h1>Hello!</h1>
+   return ( <div className={styles.wrap}>
+        <h1 className={styles.title}>Hello!</h1>
         <ItemList items = {items}/>
-        <Footer count = {3} />
+        <Footer count = {1} />
     </div>);
 }
 export default App;
