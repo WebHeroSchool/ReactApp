0a4944f34cf3b807468f231d3a5ba6bec1d5b92b
Sveta Mitukhina <s.tu.tu.908@gmail.com>
2019-12-15 19:15:44 +0300

    #40: запросы в React

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import styles from './Contact.module.css';
+import CardContent from '@material-ui/core/CardContent';
+import CircularProgress from "@material-ui/core/CircularProgress";
 import classnames from 'classnames';
 
-
-const Contacts = () => (
-    <h1 className={styles.title}>Contacts</h1>
-);
-
-export default  Contacts;
+class Contacts extends React.Component {
+  render() {
+    return (
+        <CardContent>
+          <h1 className={styles.title}>Contacts</h1>
+        </CardContent>
+    );
+  }
+}
+export default Contacts;
